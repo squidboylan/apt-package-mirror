4b442b95626684bde776d45767d6bd4ce93e334e
Caleb Boylan <calebboylan@gmail.com>
2016-07-16 22:30:08 -0700

    Check hashes of indices

--- ubuntu-mirrorpy/mirror.py
+++ ubuntu-mirrorpy/mirror.py
@@ -4,6 +4,7 @@ import urllib
 import gzip
 import yaml
 import os
+import hashlib
 from subprocess import Popen, STDOUT, PIPE
 import sys
 
@@ -17,41 +18,16 @@ class Mirror:
     def sync(self):
         self.update_mirrors()
         self.get_dists_indices()
+        self.check_release_files()
         self.check_indices()
         self.update_indices()
+        self.update_project_dir()
         self.gen_lslR()
-
-    def get_indices(self):
-        rsync_command = "rsync --recursive --times --links --hard-links -vPz rsync://{mirror_url}:/ubuntu/indices/ {mirror_path}/indices"
-        rsync_command = rsync_command.format(
-                mirror_url=self.mirror_url,
-                mirror_path=self.mirror_path
-            )
-        rsync_status = Popen(rsync_command, stdout=PIPE, stderr=PIPE,
-                shell=True)
-
-        for line in rsync_status.stdout:
-            print(line)
 
     def update_mirrors(self):
         rsync_command = "rsync --recursive --times --links --hard-links \
                 --exclude 'Packages*' --exclude 'Sources*' --exclude 'Release*' \
                 -vPz rsync://{mirror_url}:/ubuntu/ {mirror_path}"
-        rsync_command = rsync_command.format(
-                mirror_url=self.mirror_url,
-                mirror_path=self.mirror_path
-            )
-
-        rsync_status = Popen(rsync_command, stdout=PIPE, stderr=PIPE,
-                shell=True)
-
-        for line in rsync_status.stdout:
-            print(line)
-
-    def get_dists(self):
-        rsync_command = "rsync --recursive --times --links --hard-links \
-                --exclude 'Packages*' --exclude 'Sources*' --exclude 'Release*' \
-                -vPz rsync://{mirror_url}:/ubuntu/dists {mirror_path}"
         rsync_command = rsync_command.format(
                 mirror_url=self.mirror_url,
                 mirror_path=self.mirror_path
@@ -78,20 +54,35 @@ class Mirror:
         for line in rsync_status.stdout:
             print(line)
 
+    def update_project_dir(self):
+        rsync_command = "rsync --recursive --times --links --hard-links \
+                --delete -vPz rsync://{mirror_url}:/ubuntu/project \
+                {mirror_path} && date -u > ${mirror_path}/project/trace/$(hostname -f)"
+
+        rsync_command = rsync_command.format(
+                mirror_url=self.mirror_url,
+                mirror_path=self.mirror_path
+            )
+
+        rsync_status = Popen(rsync_command, stdout=PIPE, stderr=PIPE,
+                shell=True)
+
+        for line in rsync_status.stdout:
+            print(line)
+
     def check_indices(self):
         dists_path = self.temp_indices
-        indices = self._check_indices(dists_path)
+        indices = self._get_indices(dists_path)
         for index in indices:
             self.check_index(index)
             #print(index)
 
-    def _check_indices(self, dir):
-        #print(dir)
+    def _get_indices(self, dir):
         if not os.path.isfile(dir):
             indices = []
             for item in os.listdir(dir):
                 file_path = os.path.join(dir, item)
-                indices = indices + self._check_indices(file_path)
+                indices = indices + self._get_indices(file_path)
 
             return indices
 
@@ -101,7 +92,57 @@ class Mirror:
             else:
                 return []
 
+    def check_release_files(self):
+        release_files = self._get_release_files(self.temp_indices)
+        for file in release_files:
+            self.check_release_file(file)
 
+    def _get_release_files(self, dir):
+        if not os.path.isfile(dir):
+            indices = []
+            for item in os.listdir(dir):
+                file_path = os.path.join(dir, item)
+                indices = indices + self._get_release_files(file_path)
+
+            return indices
+
+        else:
+            if dir.endswith("/Release"):
+                return [dir]
+            else:
+                return []
+
+    def check_release_file(self, file_name):
+        with open(file_name) as f_stream:
+            f_contents = f_stream.read()
+
+        dir = os.path.split(file_name)[0]
+
+        hash_type = None
+        for line in f_contents.split('\n'):
+            if line.startswith("MD5Sum"):
+                hash_type = "MD5Sum"
+
+            elif line.startswith("SHA1"):
+                hash_type = "SHA1"
+
+            elif line.startswith("SHA256"):
+                hash_type = "SHA256"
+
+            elif hash_type == "MD5Sum":
+                file_to_check = line.split()[2]
+                md5sum = line.split()[0]
+                file_path = os.path.join(dir, file_to_check)
+
+                if os.path.isfile(file_path):
+                    print("Found file: " + file_path)
+                    with open(file_path, 'r') as f_stream:
+                        file_path_contents = f_stream.read()
+
+                    actual_md5sum = hashlib.md5(file_path_contents).hexdigest()
+                    if md5sum != actual_md5sum:
+                        print(actual_md5sum + ' does not match ' + md5sum)
+                        sys.exit(1)
 
     def check_index(self, file_name):
         with gzip.open(file_name) as f_stream:
