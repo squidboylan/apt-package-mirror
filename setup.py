a082450c9e6987903fbbf805d2305b0bba0df8cf
Caleb Boylan <caleb.boylan@dreamhost.com>
2016-07-21 15:00:30 -0700

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
         description = 'Python script for running an apt package mirror',
         author_email = 'calebboylan@gmail.com',
         url = 'https://github.com/squidboylan/apt-package-mirror',
-        version = '0.1.2',
+        version = '0.2.0',
         classifiers = [
             'Development Status :: 3 - Alpha',
             'Intended Audience :: System Administrators',
